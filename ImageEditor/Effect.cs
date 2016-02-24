72742d31fa6aedd2da7802bc83084f6b58f89123
Vladimir Kubantsev <kubantsev.vladimir@gmail.com>
2016-02-25 00:10:20 +0300

    Add abstract Effect class

---
+++ ImageEditor/Effect.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImageEditor
+{
+    abstract class Effect
+    {
+        public String name;
+        private Bitmap prevImage;
+        private Bitmap newImage;
+
+        abstract public Bitmap apply(Bitmap image);
+        abstract public Bitmap reverse();
+
+    }
+}
